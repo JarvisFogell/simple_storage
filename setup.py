a0aef43b5c77044f1504fde5e45d2537159defe6
Jarvis Fogell <jarvis.fogell@gmail.com>
2019-07-31 13:56:25 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='storage',
+      version='0.0.1',
+      description='Simple storage for python applications',
+      url='https://github.com/JarvisFogell/simple_storage',
+      author='Egor Pleshakov',
+      author_email='egor.pleshakov@gmail.com',
+      license='MIT',
+      packages=['storage'],
+      zip_safe=False)
